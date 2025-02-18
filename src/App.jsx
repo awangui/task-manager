a66f493c18362482673692fcc59ca0c98c34545f
Anita Wangui <anitawangui22@gmail.com>
2025-02-18 09:15:10 +0300

    add local storage and dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [tasks, setTasks]=useState([]);
+  const [tasks, setTasks]=useState(()=>{
+    return JSON.parse(localStorage.getItem("tasks"))||
+    [];
+  });
   const [newTask, setNewTask]= useState("");
+  const [darkMode, setDarkMode]=useState(false);
   //function to add a new task
   const addTask=()=>{
     if(newTask.trim() !== ""){//make sure task is not empty
@@ -11,6 +15,10 @@ function App() {
       setNewTask("")//clear input after adding task
     }
   }
+  //load tasks
+  useEffect(()=>{
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  },[tasks]);
   //function to delete task
   const deleteTask=(index)=>{
     const filteredTasks=tasks.filter((_,i)=> i !==index)
@@ -22,11 +30,14 @@ function App() {
     updatedTasks[index].completed = !updatedTasks[index].completed;
     setTasks(updatedTasks);
   }
-  
   return (
-    <div className='app'>
+    <div className={`app ${darkMode ? "dark" : ""}`}>
+        <span className="dark-mode-toggle" onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? "☀️" : "🌙 "}
+      </span>
       <h1>Task Manager 📝</h1>
       <p>Track your tasks with ease!</p>
+    
     {/* form to add a task */}
       <div className='input-form'>
         <label value="Add new task:" ></label>
